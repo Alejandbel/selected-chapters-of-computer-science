670164535b83b14bd92f31069e89f43d70924211
alejandbel <bogdanov28c@gmail.com>
2023-02-18 11:53:18 +0300

    [LAB-1] Add evens extraction script

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,5 +1,6 @@
 from hello import hello_world
 from calculations import OPERATION, calculate
+from extract_even import extract_even
 
 
 def main():
@@ -10,6 +11,10 @@ def main():
         print('Result of mult of 5 and 6:', result)
     except ZeroDivisionError:
         print('Error: division by zero')
+
+    list_of_numbers = [i for i in range(20)]
+    filtered_list = extract_even(list_of_numbers)
+    print('Result of extracting: ', filtered_list)
 
 
 if __name__ == '__main__':
