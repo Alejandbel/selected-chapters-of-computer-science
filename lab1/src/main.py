e896a4e89f2ce7789a77f991c4bb0d0eb5cbf113
alejandbel <bogdanov28c@gmail.com>
2023-02-18 11:45:18 +0300

    [LAB-1] Add calculations script

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,9 +1,16 @@
 from hello import hello_world
+from calculations import OPERATION, calculate
 
 
 def main():
     hello_world()
 
+    try:
+        result = calculate(5, 6, OPERATION.MULT)
+        print('Result of mult of 5 and 6:', result)
+    except ZeroDivisionError:
+        print('Error: division by zero')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
