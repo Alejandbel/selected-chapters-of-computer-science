c47060cf89e95865e16082a753834c1e76617fbf
alejandbel <bogdanov28c@gmail.com>
2023-02-20 01:12:30 +0300

    [LAB-2] Extract app initialization to own class

--- lab2/task2/src/main.py
+++ lab2/task2/src/main.py
@@ -1,23 +1,11 @@
-from cli import CLI
-from storage_controller import StorageController
+from app import App
 
 
 def main():
-    controller = StorageController()
-
-    cli = CLI()
-
-    cli.add_command('add', controller.add)
-    cli.add_command('list', controller.list)
-    cli.add_command('remove', controller.remove)
-    cli.add_command('find', controller.find)
-    cli.add_command('grep', controller.grep)
-    cli.add_command('exit', controller.exit)
-    cli.add_command('switch', controller.switch)
-    cli.add_command('save', controller.save)
-    cli.add_command('load', controller.load)
-
-    cli.start_application()
+    try:
+        App().start_application()
+    except KeyboardInterrupt:
+        print('\nThank you for using our application! Bye!')
 
 
 if __name__ == "__main__":
